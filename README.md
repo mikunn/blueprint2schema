85cd09cba1f8f5c4f6252f72d318b7da7a5e3c4b
Mika Kunnas <mika.kunnas@viidakko.fi>
2017-04-12 11:22:13 +0300

    first release

---
+++ README.md
@@ -0,0 +1,105 @@
+# blueprint2schema
+Build a JSON Schema tree from an API Blueprint spec file. The tree is similar to a subset of an [OpenAPI](https://github.com/OAI/OpenAPI-Specification) spec file. The `include` directive of [Aglio](https://github.com/danielgtaylor/aglio) is supported.
+
+Note that the underlying [API Blueprint parser](https://github.com/apiaryio/snowcrash) doesn't support indentation with tabs. This tool supports conversion from tabs to spaces (Linux and OSX only using `expand`) before the parsing phase.
+
+# Installation
+`npm install -g blueprint2schema`
+
+# Usage
+
+Given the following API Blueprint file called `cat.apib`
+
+```
+FORMAT: 1A
+
+## Cat [/cat]
+
+### Create cat [POST]
+Post a cat.
+
++ Request (application/json)
+
+    + Attributes (Cat)
+
++ Response 201 (application/json)
+
+    + Attributes (Cat)
+        + id: 1 (number, required)
+
+# Data Structures
+
+## Cat
+
++ name: Catty (string, required)
+```
+
+the following will be ouputted to `stdout`:
+
+```shell
+$ blueprint2schema -i cat.apib -p
+{
+  "/cat": {
+    "post": {
+      "body": {
+        "schema": {
+          "$schema": "http://json-schema.org/draft-04/schema#",
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "name"
+          ]
+        }
+      },
+      "responses": {
+        "201": {
+          "body": {
+            "schema": {
+              "$schema": "http://json-schema.org/draft-04/schema#",
+              "type": "object",
+              "properties": {
+                "name": {
+                  "type": "string"
+                },
+                "id": {
+                  "type": "number"
+                }
+              },
+              "required": [
+                "name",
+                "id"
+              ]
+            }
+          }
+        }
+      }
+    }
+  }
+}
+```
+
+If option `-i` is not given, the input file is read from `stdin`.
+
+
+## Options
+
+```shell
+$ blueprint2schema -h
+
+  Usage: blueprint2schema [options]
+
+  Options:
+
+    -h, --help              output usage information
+    -i, --input [filepath]  API Blueprint file
+    -t, --expand-tabs       Expand tabs to spaces (Linux and OSX only)
+    -p, --pretty-print      Enable pretty printing
+```
+
+# License
+
+MIT
---
+++ index.js
@@ -0,0 +1,175 @@
+#!/usr/bin/env node
+
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+var path = require('path');
+
+var program = require('commander');
+var fury = require('fury');
+var apibParser = require('fury-adapter-apib-parser');
+var apibIncludeDirective = require('apib-include-directive');
+
+var apibData = '',
+	includeDir	
+;
+
+fury.use(apibParser);
+
+program
+	.option('-i, --input [filepath]', 'API Blueprint file')
+	.option('-t, --expand-tabs', "Expand tabs to spaces (Linux and OSX only)")
+	.option('-p, --pretty-print', "Enable pretty printing")
+	.parse(process.argv)
+;
+
+includeDir = program.input ? path.dirname(program.input) : process.cwd();
+
+(program.input ? fs.createReadStream(program.input) : process.stdin)
+	.on('data', function(chunk) {
+		apibData += chunk;
+	})
+	.on('end', function() {
+		var data, spaces;
+
+		try {
+			apibData = apibIncludeDirective.includeDirective(includeDir, apibData);
+		} catch (err) {
+			console.error(err.toString());
+			return;
+		}
+
+		if (! program.expandTabs) {
+			parseApib(apibData, print);
+		} else {
+			expandTabs(apibData, function(err, data) {
+				if (err) {
+					console.error(err.toString());
+				} else {
+					parseApib(data, print);	
+				}
+			});
+		}
+
+	})
+;
+
+function print(data) {
+	spaces = program.prettyPrint ? 2 : null;
+	console.log(JSON.stringify(data, null, spaces));
+}
+
+function getContents(data, href, transaction) {
+	var responsesDef,
+		request = transaction.request,
+		response = transaction.response,
+		method = request.method.toLowerCase()
+	;
+
+	href = request.href ? request.href : href;
+	
+	// replace parameters from endpoint spec
+	href = href.replace(/\{\?.+\}/, '');
+
+	if (! data.hasOwnProperty(href)) {
+		data[href] = {};
+	}
+
+	if (! data[href].hasOwnProperty(method)) {
+		data[href][method] = {
+			'body': buildBody(request),
+			'responses': {}
+		};
+	}
+
+	responsesDef = data[href][method]['responses'];
+	buildResponses(responsesDef, response);
+
+	return data;
+}
+
+function buildResponses(responsesDef, response) {
+	if (! response.messageBodySchema) {
+		responsesDef[response.statusCode] = {};
+		return responsesDef;
+	}
+	responsesDef[response.statusCode] = {
+		body: {
+			schema: parseSchemaContent(response.messageBodySchema.content)
+		}
+	};
+
+	return responsesDef;
+}
+
+function buildBody(request) {
+	if (! request.messageBodySchema) {
+		return {};
+	}
+	return {
+		schema: parseSchemaContent(request.messageBodySchema.content)
+	}
+}
+
+function parseSchemaContent(content) {
+	if (typeof content == 'string') {
+		content = JSON.parse(content);
+	}
+	return content;	
+}
+
+function parseApib(source, callback) {
+	var data = {};
+
+	fury.parse({source}, function(err, result) {
+		var api = result.api,
+			href = ''
+		;
+
+		if (err) {
+			console.error(err.toString() +
+				" Try using -t to convert tabs to spaces if you're on Linux or OSX");
+			throw err;
+
+		} else {
+			api.resourceGroups.forEach(function(resourceGroup) {
+				resourceGroup.resources.forEach(function(resource) {
+					href = resource.href;
+			 
+					resource.transitions.forEach(function(transition) {
+						href = transition.href ? transition.href : href;
+
+						transition.transactions.forEach(function(transaction) {
+							data = getContents(data, href, transaction);
+						});
+					});
+				});
+			});	
+			callback(data);
+		}
+		
+	});
+}
+
+function expandTabs(apibData, callback) {
+	var expand = spawn('expand', ['-t', '4']);
+
+	expand.stdin.write(apibData);
+	expand.stdin.end();
+
+	apibData = '';
+
+	expand.stdout.on('data', function(data) {
+		apibData += data;
+	});
+	expand.stderr.on('data', function(data) {
+		console.error(data.toString());
+	});
+	expand.on('close', function(code) {
+		var err = null;
+
+		if (code > 0) {
+			err = new Error('expand failed with error code ' + code);	
+		}
+		callback(err, apibData);
+	});
+}
